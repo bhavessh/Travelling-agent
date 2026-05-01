762635b7b96f5d3c3c9a18ee1210fca684978299
bhavessh <jbhavesh.in@gmail.com>
2026-05-02 02:06:59 +0530

    final upload

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ovco9b5jyh.ufs.sh",
+      },
+      {
+        hostname: "btawlusa03.ufs.sh",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
